ae1583aa42ce38580d940807b62764c74815aaf7
willmusubi <willmusubi@gmail.com>
2023-03-10 00:48:51 -0800

    added verify

--- hardhat-erc20/deploy/01-deploy-token.js
+++ hardhat-erc20/deploy/01-deploy-token.js
@@ -1,27 +1,30 @@
 const { network } = require("hardhat");
 const {
-  INITIAL_SUPPLY,
-  developmentChains,
+    INITIAL_SUPPLY,
+    developmentChains,
 } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts();
-  const args = [INITIAL_SUPPLY];
-  const willToken = await deploy("WillToken", {
-    from: deployer,
-    args: args,
-    log: true,
-    waitConfirmations: network.config.blockConfirmations || 1,
-  });
+    console.log;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const args = [INITIAL_SUPPLY];
+    const willToken = await deploy("WillToken", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
 
-  log(`willToken deployed at ${willToken.address}`);
+    log(`willToken deployed at ${willToken.address}`);
 
-  if (
-    !developmentChains.includes(network.name) &&
-    process.env.ETHERSCAN_API_KEY
-  ) {
-    await verify(willToken.address, args);
-  }
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(willToken.address, args);
+    }
 };
+
+module.exports.tags = ["all", "token"];
